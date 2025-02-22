f89089b21572692952f832a0e3965cb4ee880e59
Leon_lp9 <leon.marcel.rabe@gmail.com>
2025-02-22 13:17:54 +0100

    Update inventory display and adjust layout for responsiveness

--- skilltree/script.js
+++ skilltree/script.js
@@ -12,32 +12,63 @@ const heros = [
                         name: "Max Duration",
                         description: "The maximin time you can hold the ability",
                         icon: "url(../item/clock.png)",
+                        position: { x: -2, y: 3 },
+                        challengeType: "task",
+                        requirements: [
+                            {
+                                id: "max_duration",
+                                value: 1,
+                            }
+                        ],
                     }
-                ]
+                ],
+                position: { x: 0, y: 1 },
+                challengeType: "challenge",
             },
             tornado = {
                 name: "Tornado",
                 description: "Hab das noch nicht ingame gesehen",
                 background: "url(../block/white_wool.png)",
                 icon: "url(../item/feather.png)",
+                skills: [
+
+                ],
+                position: { x: 0, y: 1 },
+                challengeType: "challenge",
             },
             air_ball = {
                 name: "Air Ball",
                 description: "By making circular movements with the mouse, you can create an air ball that can be scaled by scrolling.",
                 background: "url(../block/raw_iron_block.png)",
                 icon: "url(../item/wind_charge.png)",
+                skills: [
+
+                ],
+                position: { x: 0, y: 1 },
+                challengeType: "challenge",
             },
             levitation = {
                 name: "Levitation",
                 description: "While holding the ability, you can glide.",
                 background: "url(../block/packed_ice.png)",
                 icon: "url(../item/feather.png)",
+                skills: [
+
+                ],
+                position: { x: 0, y: 1 },
+                challengeType: "challenge",
+
             },
             spiritual_projection = {
                 name: "Spiritual Projection",
                 description: "Hab das noch nicht ingame gesehen",
                 background: "url(../block/bricks.png)",
                 icon: "url(../item/feather.png)",
+                skills: [
+
+                ],
+                position: { x: 0, y: 1 },
+                challengeType: "challenge",
             },
         ]
     },
@@ -48,6 +79,11 @@ const heros = [
 
     },
 ]
+
+const urlParams = new URLSearchParams(window.location.search);
+const uuid = urlParams.get('uuid');
+
+let stats = null;
 
 function renderSkillTree(hero) {
     const tabs = hero.tabs;
@@ -102,16 +138,41 @@ function renderSkillTree(hero) {
         tabsOpenContainer.querySelectorAll('button').forEach((button, i) => {
             button.style.opacity = i === index ? '1' : '0';
         });
+
+        content.scrollLeft = content.scrollWidth / 2 - content.clientWidth / 2;
+        content.scrollTop = 0;
+
+        document.getElementById("skills").innerHTML = '';
+        placeSkill(selectedTab);
+        selectedTab.skills.forEach(skill => {
+            placeSkill(skill);
+        });
     }
 
-    // Open the first tab by default
+    function placeSkill(skill) {
+        const skillElement = document.createElement('div');
+        skillElement.classList.add('skill');
+        skillElement.style.left = `calc(50% + ${skill.position.x * 50}px)`;
+        skillElement.style.top = `${skill.position.y * 50 + 50}px`;
+        skillElement.classList.add(skill.challengeType);
+
+        //skill title
+        const skillTitle = document.createElement('div');
+        skillTitle.classList.add('title');
+        skillTitle.innerText = skill.name;
+        skillElement.appendChild(skillTitle);
+
+        const skillIcon = document.createElement('div');
+        skillIcon.classList.add('icon');
+        skillIcon.style.backgroundImage = skill.icon;
+        skillElement.appendChild(skillIcon);
+
+        document.getElementById("skills").appendChild(skillElement);
+    }
+
     openTab(0);
+
 }
-
-const urlParams = new URLSearchParams(window.location.search);
-const uuid = urlParams.get('uuid');
-
-let stats = null;
 async function fetchStats() {
     const response = await fetch(`https://api.hglabor.de/stats/ffa/${uuid}`);
     stats = await response.json();
@@ -120,45 +181,45 @@ async function fetchStats() {
     renderSkillTree(heros[0]);
 }
 
+function enableDragScroll(element) {
+    let isDown = false;
+    let startX;
+    let scrollLeft;
+    let startY;
+    let scrollTop;
+
+    element.addEventListener('mousedown', (e) => {
+        isDown = true;
+        element.classList.add('active');
+        startX = e.pageX - element.offsetLeft;
+        scrollLeft = element.scrollLeft;
+        startY = e.pageY - element.offsetTop;
+        scrollTop = element.scrollTop;
+    });
+
+    element.addEventListener('mouseleave', () => {
+        isDown = false;
+        element.classList.remove('active');
+    });
+
+    element.addEventListener('mouseup', () => {
+        isDown = false;
+        element.classList.remove('active');
+    });
+
+    element.addEventListener('mousemove', (e) => {
+        if (!isDown) return;
+        e.preventDefault();
+        const x = e.pageX - element.offsetLeft;
+        const walkX = (x - startX);
+        element.scrollLeft = scrollLeft - walkX;
+
+        const y = e.pageY - element.offsetTop;
+        const walkY = (y - startY);
+        element.scrollTop = scrollTop - walkY;
+    });
+}
+
 fetchStats().then(() => {});
 
-
-
-// Enable both horizontal and vertical scrolling with mouse drag
-const content = document.getElementById('content');
-let isDown = false;
-let startX;
-let scrollLeft;
-let startY;
-let scrollTop;
-
-content.addEventListener('mousedown', (e) => {
-    isDown = true;
-    content.classList.add('active');
-    startX = e.pageX - content.offsetLeft;
-    scrollLeft = content.scrollLeft;
-    startY = e.pageY - content.offsetTop;
-    scrollTop = content.scrollTop;
-});
-
-content.addEventListener('mouseleave', () => {
-    isDown = false;
-    content.classList.remove('active');
-});
-
-content.addEventListener('mouseup', () => {
-    isDown = false;
-    content.classList.remove('active');
-});
-
-content.addEventListener('mousemove', (e) => {
-    if (!isDown) return;
-    e.preventDefault();
-    const x = e.pageX - content.offsetLeft;
-    const walkX = (x - startX);
-    content.scrollLeft = scrollLeft - walkX;
-
-    const y = e.pageY - content.offsetTop;
-    const walkY = (y - startY);
-    content.scrollTop = scrollTop - walkY;
-});
+enableDragScroll(document.getElementById('content'));
